af10bec566c9c87beb9d3e6d0bd628d593f2c846
zhuyangzou <zouminghao123@163.com>
2020-08-12 21:06:00 +1200

    Zouzhuyang-12.08

---
+++ ICT638June2020Group03Android/Activities/RegisterActivity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ICT638June2020Group03Android.Activities
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.activity_add_user_details);
+            base.OnCreate(savedInstanceState);
+
+            Button btn_add = FindViewById<Button>(Resource.Id.btn_register_add);
+            btn_add.Click += Btn_add_Click;
+            Button btn_back = FindViewById<Button>(Resource.Id.btn_home);
+            btn_back.Click += Btn_back_Click;
+
+            // Create your application here
+        }
+
+        private void Btn_back_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+
+        public string getQuotedString(string str)
+        {
+            return "\"" + str + "\"";
+        }
+
+        private void Btn_add_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.FirstName = FindViewById<EditText>(Resource.Id.txt_first_name_add).Text;
+            user.LastName = FindViewById<EditText>(Resource.Id.txt_last_name_add).Text;
+            user.Password = FindViewById<EditText>(Resource.Id.txt_password_add).Text;
+            user.EmailAddress = FindViewById<EditText>(Resource.Id.txt_email_address_add).Text;
+            user.PhoneNumber = FindViewById<EditText>(Resource.Id.txt_phone_number_add).Text;
+            user.Address = FindViewById<EditText>(Resource.Id.txt_address_add).Text;
+            user.Country = FindViewById<EditText>(Resource.Id.txt_country_add).Text;
+
+            string url = "https://10.0.2.2:5001/api/Users";
+            var httpWebRequest = new HttpWebRequest(new Uri(url));
+            //var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ServerCertificateValidationCallback = delegate { return true; };
+            //httpWebRequest.ServerCertificateCustomValidationCallback = delegate { return true; }
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{" +
+                    getQuotedString("FirstName") + ":" + getQuotedString(user.FirstName) + "," +
+                    getQuotedString("LastName") + ":" + getQuotedString(user.LastName) + "," +
+                    getQuotedString("Password") + ":" + getQuotedString(user.Password) + "," +
+                    getQuotedString("EmailAddress") + ":" + getQuotedString(user.EmailAddress) + "," +
+                    getQuotedString("PhoneNumber") + ":" + getQuotedString(user.PhoneNumber) + "," +
+                    getQuotedString("Address") + ":" + getQuotedString(user.Address) + "," +
+                    getQuotedString("Country") + ":" + getQuotedString(user.Country) +
+                               "}";
+
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
--- ICT638June2020Group03Android/MainActivity.cs
+++ ICT638June2020Group03Android/MainActivity.cs
@@ -3,26 +3,71 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Net;
+using System.IO;
+using System.Text;
+using System;
+using Newtonsoft.Json;
+using ICT638June2020Group03Android.Activities;
 
 namespace ICT638June2020Group03Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
-
-    //ben
+    {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_login);
+
+            Button btn_login = FindViewById<Button>(Resource.Id.btn_login);
+            btn_login.Click += Btn_login_Click;
+
+            Button btn_signin = FindViewById<Button>(Resource.Id.btn_register_page);
+            btn_signin.Click += Btn_signin_Click;
         }
+
+        private void Btn_signin_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void Btn_login_Click(object sender, System.EventArgs e)
+        {
+            EditText edEmail = FindViewById<EditText>(Resource.Id.txt_email_address_del);
+            EditText edPassword = FindViewById<EditText>(Resource.Id.txt_password_del);
+
+            string url = "https://10.0.2.2:5001/api/Users/1";
+            string result = "";
+            var httpWebRequest = new HttpWebRequest(new Uri(url));
+            httpWebRequest.ServerCertificateValidationCallback = delegate { return true; };
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "Get";
+
+
+
+            HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            User user = new User();
+            user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(result);
+
+            if (edEmail.Text == user.EmailAddress && edPassword.Text == user.Password)
+            {
+                StartActivity(typeof(DetailActivity));
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
     }
 }
