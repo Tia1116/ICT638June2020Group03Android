fb49c56bfd26dd692690e1ca3a8153027a827ead
Harry664 <807563511@qq.com>
2020-08-17 03:24:37 +0800

    all project

---
+++ ICT638June2020Group03Android/Navigation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace ICT638June2020Group03Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class Navigation : Activity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        TextView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.House);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.fragContainer);
+            navigation.SetOnNavigationItemSelectedListener(this);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        [System.Obsolete]
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            houseFragment houseFragment = new houseFragment();
+
+            FrameLayout fragCon = FindViewById<FrameLayout>(Resource.Id.fragContainer);
+            FragmentTransaction transaction;
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_House:
+                    textMessage.SetText(Resource.String.title_House);
+                    fragCon.RemoveAllViewsInLayout();
+                    fragCon.RemoveViews(0, fragCon.ChildCount);
+                    transaction = FragmentManager.BeginTransaction();
+                    transaction.Replace(Resource.Id.fragContainer, houseFragment, "Home");
+                    transaction.AddToBackStack("Home");
+                    transaction.Commit();
+
+                    return true;
+                case Resource.Id.navigation_agent:
+                    textMessage.SetText(Resource.String.title_agent);
+                    transaction = FragmentManager.BeginTransaction();
+                    transaction.Replace(Resource.Id.fragContainer, houseFragment, "agent");
+                    transaction.AddToBackStack("agent");
+                    transaction.Commit();
+                    return true;
+                case Resource.Id.navigation_User:
+                    textMessage.SetText(Resource.String.title_User);
+                    transaction = FragmentManager.BeginTransaction();
+                    transaction.Replace(Resource.Id.fragContainer, houseFragment, "User");
+                    transaction.AddToBackStack("User");
+                    transaction.Commit();
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ICT638June2020Group03Android/houseActivity.cs
@@ -0,0 +1,259 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Widget;
+using Xamarin.Essentials;
+using System;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using System.Linq;
+using Android.Support.V7.App;
+using Android.Views;
+using System.Net;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+
+namespace ICT638June2020Group03Android
+{
+    [Activity(Label = "houseActivity", MainLauncher = true)]
+    public class houseActivity : Activity, IOnMapReadyCallback
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.House);
+            Button btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn1.Click += Btn1_Click; ;
+            Button btn2 = FindViewById<Button>(Resource.Id.button2);
+            btn2.Click += Btn2_Click; ;
+            var mapFrag = MapFragment.NewInstance();// mapOptions);
+
+
+            FragmentManager.BeginTransaction()
+                                       .Add(Resource.Id.mapContainer1, mapFrag, "map_fragment")
+                                       .Commit();
+
+            mapFrag.GetMapAsync(this);
+        }
+
+        public void getHouseDetail()
+
+        {
+
+            string url = "https://localhost:5001/api/House/1";
+
+            var httpWebRequest = new HttpWebRequest(new Uri(url));
+
+            httpWebRequest.ServerCertificateValidationCallback = delegate { return true; };
+
+            httpWebRequest.ContentType = "application/json";
+
+            httpWebRequest.Method = "GET";
+
+
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+
+            {
+
+                var result = streamReader.ReadToEnd();
+
+                JsonConvert.DeserializeObject<House>(result);
+
+            }
+
+
+        }
+        private async void Btn1_Click(object sender, System.EventArgs e)
+        {
+            TextView Address = FindViewById<TextView>(Resource.Id.textView3);
+            TextView Rentfees = FindViewById<TextView>(Resource.Id.textView2);
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = "Hi, I am interested in the house at" + Address.Text + "you have posted for rent. Could I please have more details?",
+                Title = "Share text"
+            });
+
+
+        }
+        private async void Btn2_Click(object sender, System.EventArgs e)
+        {
+            TextView bathroomNumber = FindViewById<TextView>(Resource.Id.textView1);
+            TextView Rentfees = FindViewById<TextView>(Resource.Id.textView2);
+            TextView Address = FindViewById<TextView>(Resource.Id.textView3);
+            string text = "Hi, The bathroomNumber is" + bathroomNumber.Text + " saw your details on the Rent - a - go app.Could you please send me details of more houses for rent in the same price range ?";
+
+            try
+            {
+                var message = new SmsMessage(text, Rentfees.Text);
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        // Other error has occurred.
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        [System.Obsolete]
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            googleMap.MapType = GoogleMap.MapTypeNormal;
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+
+            getCurrentLoc(googleMap);
+        }
+
+        public async void getLastLocation(GoogleMap googleMap)
+        {
+            Console.WriteLine("Test - LastLoc");
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location != null)
+                {
+                    Console.WriteLine($"Last Loc - Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    MarkerOptions curLoc = new MarkerOptions();
+                    curLoc.SetPosition(new LatLng(location.Latitude, location.Longitude));
+                    var address = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = address?.FirstOrDefault();
+                    var geocodeAddress = "";
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n" +
+                            $"PostalCode:      {placemark.PostalCode}\n" +
+                            $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                            $"SubLocality:     {placemark.SubLocality}\n" +
+                            $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                            $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    }
+                    curLoc.SetTitle("You were here" + geocodeAddress);
+                    curLoc.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure));
+                    googleMap.AddMarker(curLoc);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                Toast.MakeText(this, "Feature Not Supported", ToastLength.Short);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                Toast.MakeText(this, "Feature Not Enabled", ToastLength.Short);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                Toast.MakeText(this, "Needs more permission", ToastLength.Short);
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                Toast.MakeText(this, "Unable to get location", ToastLength.Short);
+            }
+        }
+
+        public async void getCurrentLoc(GoogleMap googleMap)
+        {
+            Console.WriteLine("Test - CurrentLoc");
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"current Loc - Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    MarkerOptions curLoc = new MarkerOptions();
+                    curLoc.SetPosition(new LatLng(location.Latitude, location.Longitude));
+
+
+                    var address = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = address?.FirstOrDefault();
+                    var geocodeAddress = "";
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n";
+
+                    }
+
+
+                    curLoc.SetTitle("house location");// + geocodeAddress);
+                    curLoc.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure));
+
+                    googleMap.AddMarker(curLoc);
+
+
+                    CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                    builder.Target(new LatLng(location.Latitude, location.Longitude));
+                    builder.Zoom(18);
+                    builder.Bearing(155);
+                    builder.Tilt(65);
+
+                    CameraPosition cameraPosition = builder.Build();
+
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+
+                    googleMap.MoveCamera(cameraUpdate);
+                }
+                else
+                {
+                    getLastLocation(googleMap);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                Toast.MakeText(this, "Feature Not Supported", ToastLength.Short);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                Toast.MakeText(this, "Feature Not Enabled", ToastLength.Short);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                Toast.MakeText(this, "Needs more permission", ToastLength.Short);
+            }
+            catch (Exception ex)
+            {
+                getLastLocation(googleMap);
+            }
+        }
+    }
+}
